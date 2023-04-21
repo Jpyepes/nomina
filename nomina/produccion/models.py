a46e3b9128204f98dc11474ec93492f7698c3535
jpyepes <jpyg123@gmail.com>
2023-04-20 20:26:01 -0500

    Adelanto app produccion

--- nomina/produccion/models.py
+++ nomina/produccion/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    stockUnidades = models.PositiveIntegerField(default=0)
+    stockGramos = models.FloatField(default=0)
+    precio = models.PositiveIntegerField(default=0)
+
+class ProductoOrden(models.Model):
+    cantidadSolicitada = models.PositiveIntegerField()
+    productos = models.ManyToManyField(Producto)
+
+class OrdenProduccion(models.Model):
+    id = models.CharField(max_length=6, primary_key=True)
+    productoOrden = models.ManyToManyField(ProductoOrden)
+    
+    def save(self, *args, **kwargs):
+        if not self.id:
+            ultimo_id = OrdenProduccion.objects.all().order_by('-id').first()
+            if ultimo_id:
+                numero_id = int(ultimo_id.id) + 1
+            else:
+                numero_id = 1
+            self.id = str(numero_id).zfill(6)
+        super().save(*args, **kwargs)
