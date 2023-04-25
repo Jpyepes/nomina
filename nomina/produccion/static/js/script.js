25f0cf95dc20fe6f7baa34368f321588b6ca2d8c
juan <jpyg123@gmail.com>
2023-04-25 01:51:10 -0500

    Adelanto modulo produccion

--- nomina/produccion/static/js/script.js
+++ nomina/produccion/static/js/script.js
@@ -4,38 +4,21 @@ const inputMandarDatos = document.getElementById('mandarPython');
 const verInfo = document.getElementById('infoListProductos');
 var listaProductos = [];
 function agregarElemento(){
+    verInfo.innerHTML='';
     if(inputCantidad.value != ''){
-    listaProductos.push(['['+inputSelect.value,inputCantidad.value+']']);
+    listaProductos.push([inputSelect.value,inputCantidad.value]);
     console.log(listaProductos);
-    verInfo.innerHTML = listaProductos;
+    for(let i = 0;i<listaProductos.length;i++){
+        let producto = document.createElement("div");
+        producto.setAttribute("class","verInfoProducto");
+        producto.innerHTML = "Producto: "+listaProductos[i][0]+" - Cantidad: "+listaProductos[i][1];
+        verInfo.appendChild(producto);
+    }
+    //verInfo.innerHTML = listaProductos;
     inputMandarDatos.value = listaProductos;
     inputSelect.value = '';
     inputCantidad.value = '';
     } else{
-        verInfo.innerHTML = 'Por favor rellene el campo de cantidad solicitada';
+        verInfo.innerHTML = 'Por favor rellene el campo de cantidad solicitado';
     }
 }
-
-/*var listaProductos = [];
-const inputSelect = document.getElementById('inputGrande');
-function agregarProducto(){
-    let contenedor = document.getElementById('contenedorCrearOrden');
-    let section = document.getElementById('sectionPedidoOrden');
-    let nuevaSection = section.cloneNode(true);
-    contenedor.appendChild(nuevaSection);
-    let botonesEliminar = document.getElementsByClassName('borrarProductoOrden');
-    for(let i=0; i<botonesEliminar.length; i++){
-        botonesEliminar[i].addEventListener('click', eliminarProducto);
-    }
-}
-function eliminarProducto(event){
-    let contenedor = document.getElementById('contenedorCrearOrden');
-    let section = event.target.closest('.divFlex');
-    contenedor.removeChild(section);
-}
-
-inputSelect.addEventListener('change', agregarLista);
-function agregarLista(e){
-    listaProductos.push(e.value);
-    console.log(listaProductos);
-}*/
--- nomina/produccion/views.py
+++ nomina/produccion/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Producto, ProductoOrden, OrdenProduccion
+from datetime import datetime
 # Create your views here.
 
 def crearProducto (request):
@@ -18,9 +19,59 @@ def crearProducto (request):
 
 def crearOrden(request):
     if request.method == 'POST':
+        fechaEntrega = request.POST.get('fechaEntrega')
         data = request.POST.get('datosProducto')
-        listaProductos = [data]
-        print(type(data))
-        print(data[0])
-        print(listaProductos)
+        dataTratada = data.split(",")
+        productos = []
+        cantidad = []
+        orden = OrdenProduccion(fechaCreacion=datetime.now(),fechaEntrega=fechaEntrega)
+        orden.save()
+        try:
+            for i in range(len(dataTratada)):
+                if i % 2 == 0:
+                    productos.append(dataTratada[i])
+                else:
+                    cantidad.append(dataTratada[i])    
+        except:
+            print('Ha ocurrido un error, vuelva a intentarlo más tarde')
+
+        for i in range(len(productos)):
+            relacionProducto = pkNombre(productos[i])
+            varAux = ProductoOrden(cantidadSolicitada=cantidad[i],precio=calcularPrecioPO(productos[i],cantidad[i]),producto=relacionProducto,ordenProduccion=orden)
+            varAux.save()
+            relacionProducto.lote = relacionProducto.lote + 1
+            relacionProducto.save()
+
     return render(request, 'crearOrden.html')
+
+def verOrden(request):
+    ordenes = ProductoOrden.objects.all().order_by('-ordenProduccion__id')
+    datos_por_orden = {}
+    fecha_creacion = None
+    fecha_entrega = None
+    for objeto in ordenes:
+        id_orden = objeto.ordenProduccion.id
+        nombre_producto = objeto.producto.nombre
+        cantidad = objeto.cantidadSolicitada
+
+        fechas = OrdenProduccion.objects.get(id=id_orden)
+        fecha_creacion = fechas.fechaCreacion
+        fecha_entrega = fechas.fechaEntrega
+        if id_orden not in datos_por_orden:
+            datos_por_orden[id_orden] = {'productos': [],'fecha_creacion': fecha_creacion, 'fecha_entrega': fecha_entrega}
+        datos_por_orden[id_orden]['productos'].append({'nombre': nombre_producto, 'cantidad': cantidad})
+
+    # Crear un contexto con los datos de cada orden de producción
+    context = {'datos_por_orden': datos_por_orden, 'fechaCreacion': fecha_creacion,'fechaEntrega': fecha_entrega}
+    return render(request, 'verOrden.html', context)
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+def pkNombre(nombreP):
+    producto = Producto.objects.get(nombre=nombreP)
+    return producto
+def getObjOrdenes(idPO):
+    orden = OrdenProduccion.objects.get(id=idPO.id)
+    return orden
+def calcularPrecioPO(nombreP,cantidadS):
+    producto = Producto.objects.get(nombre=nombreP)
+    precio = int(producto.precio) * int(cantidadS)
+    return precio
