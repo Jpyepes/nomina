e2972fe1562d181bb2624e61f3778499bf4b3c01
jpyepes <jpyg123@gmail.com>
2023-05-10 07:44:36 -0500

    Organización del proyecto

--- nomina/nomina/settings.py
+++ nomina/nomina/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -75,11 +75,13 @@ WSGI_APPLICATION = 'nomina.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+dbpath = Path(os.path.join(BASE_DIR.parent.parent, 'entorno'))
+print(dbpath)
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': dbpath / 'db.sqlite3',
     }
 }
 
